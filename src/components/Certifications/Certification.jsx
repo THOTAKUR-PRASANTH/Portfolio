64a61696380ae4fcef16bc5aa3fed596bc144059
THOTAKUR-PRASANTH <211fa04025@vignan.ac.in>
2025-06-08 04:29:20 +0530

    update

--- src/components/Certifications/Certification.jsx
+++ src/components/Certifications/Certification.jsx
@@ -17,14 +17,15 @@ import oracle_certification from "../../Assets/Certificates/Oracle_Certificate.j
 const certificatesData = [
   { id: 1, title: "TCS CodeVita Season 11 Certificate 2024", img: TCS_CodeVita_Season_11 },
   { id: 2, title: "TCS Codevita Season 12 Global Rank Holder 2025", img: TCS_Codevita_Season_12 },
-  { id: 3, title: "E-Business", img: E_Business },
-  { id: 4, title: "Cyber Security and Privacy", img: Cyber_Security_and_Privacy },
-  { id: 5, title: "Internship Certificate", img: Internship_Certificate },
-  { id: 6, title: "Computer Vision", img: Computer_Vision },
-  { id: 7, title: "Introduction To Internet Of Things", img: Introduction_To_Internet_Of_Things },
-  { id: 8, title: "Problem Solving Through Programming In C", img: Problem_Solving_Through_Programming_In_C },
-  { id: 9, title: "Principles of Management", img: Principles_of_Management },
-  { id : 10, title: " Oracle Cloud Infrastructure 2024 Generative AI Certified Professional", img: oracle_certification }
+  { id : 3, title: " Oracle Cloud Infrastructure 2024 Generative AI Certified Professional", img: oracle_certification },
+  { id: 4, title: "Internship Certificate", img: Internship_Certificate },
+    { id: 5, title: "Problem Solving Through Programming In C", img: Problem_Solving_Through_Programming_In_C },
+     { id: 6, title: "Cyber Security and Privacy", img: Cyber_Security_and_Privacy },
+  { id: 7, title: "E-Business", img: E_Business },
+  { id: 8, title: "Computer Vision", img: Computer_Vision },
+  { id: 9, title: "Introduction To Internet Of Things", img: Introduction_To_Internet_Of_Things },
+  { id: 10, title: "Principles of Management", img: Principles_of_Management },
+  
 ];
 
 function Certification() {
